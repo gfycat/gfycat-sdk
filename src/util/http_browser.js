72b6f7fc8403a6de214ff6b2a90f4bb831d6d1ff
Josh <josh@gfycat.com>
2017-03-21 16:54:12 -0700

    Add http xhr shim for browser build

---
+++ src/util/http_browser.js
@@ -0,0 +1,64 @@
+/* globals XMLHttpRequest */
+
+/**
+ * Browser based http request
+ * @param options {Object}
+ *   options.request {Object} - Request data
+ *      request.headers {Object} - (optional) HTTP headers to be sent with the request
+ *      request.method {String} - HTTP method
+ *      request.postData {Object} - (optional) JSON data to be sent with POST request
+ *      request.url {String} - API url
+ *   options.timeout {Number} - (optional) Request timeout before returning an error. Defaults to 30000 milliseconds
+ */
+exports.request = function(options, resolve, reject) {
+  var timeout = options.timeout || 30000;
+
+  var timer = setTimeout(function() {
+    xhr.abort();
+    reject(new Error('API request exceeded timeout of', timeout));
+  }, timeout);
+
+  var xhr = new XMLHttpRequest();
+
+  function handleError(err) {
+    clearTimeout(timer);
+    err = err || new Error('API request failed');
+    reject(err);
+  }
+
+  function handleResponse(res) {
+    clearTimeout(timer);
+
+    if (xhr.status >= 400) return reject(xhr.status);
+
+    var body = xhr.response;
+    try {
+      body = JSON.parse(body);
+      resolve(body)
+    } catch (e) {
+      reject(e)
+    }
+  }
+
+  xhr.addEventListener('error', handleError);
+  xhr.addEventListener('abort', handleError);
+  xhr.addEventListener('load', handleResponse);
+
+  xhr.open(options.request.method, options.request.url, true);
+
+  var headers = options.request.headers || null;
+  if (headers) {
+    Object.keys(headers).forEach(function(header) {
+      xhr.setRequestHeader(header, headers[header])
+    })
+  }
+
+  var data = JSON.stringify(options.request.postData) || null;
+
+  if (!data) {
+    xhr.send();
+  } else {
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(data);
+  }
+}
