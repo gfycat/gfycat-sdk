28dc0bc94e947b05b04f04972ad662c3d490a984
Josh <josh@gfycat.com>
2017-03-21 20:40:33 -0700

    Add environment specific configs for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,27 @@
 const path = require('path');
 const webpack = require('webpack');
+const env = process.env.NODE_ENV || 'development';
+const __DEV__ = env === 'development';
+const __PROD__ = env === 'production';
 
 module.exports = {
   entry: './src/es5-gfycat-sdk',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'gfycat-sdk.umd.bundle.min.js',
+    filename: __DEV__ ? 'gfycat-sdk.umd.bundle.js' : 'gfycat-sdk.umd.bundle.min.js',
     publicPath: '/assets/',
     library: 'GfycatSDK',
     libraryTarget: 'umd'
   },
   devtool: 'source-map',
   target: 'web',
-  plugins: [
+  plugins: __PROD__ ? [
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false,
         drop_console: false
       }
     })
-  ]
+  ] : [],
+  watch: __DEV__
 };
